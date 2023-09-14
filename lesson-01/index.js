d0b079688b364deb08d4234ef32bb2a372a82fe2
Deep Kothari <deepk2891@gmail.com>
2023-09-14 13:25:35 +0530

    Completed lesson-01

---
+++ lesson-01/index.js
@@ -0,0 +1,44 @@
+//  in this lesson:
+// -------------------
+/*
+
+    1.  javascript is giving instructions to a computer.
+    2.  wrote javascript code.
+    3.  Run our code using the console.
+
+
+    Lesson 1 : Exercises - JavaScript Basics
+
+    !!! NOTE : Do these exercise in the console (right-click > inspect > Console)
+
+*/
+
+//  1a. Use alert(...); to display 'Good morning!' in a popup
+alert("Good morning!");
+
+//  1b. Display your nam ein a popup.
+alert("deepk2891");
+
+//  1c. Using math, calculate 10 + 5 in the Console.
+10 + 5;
+
+//  1d. Calculate 20 - 5 in the Console.
+20 - 5;
+
+//  1e. Calculate 2 + 2 - 5 in the Console.
+2 + 2 - 5;
+
+//  1f. Use document.body.innerHTML=...;  to display 'Good morning!' on the web page.
+document.body.innerHTML = "Good morning!";
+
+//  1g. Display your name on the webpage.
+document.body.innerHTML = "deepk2891";
+
+//  1h. You order T-shirt for $10, socks for $8 and dinner plates for $20. Use JavaScript to calculate the total cost of your order.
+10 + 8 + 20;
+
+//  1i. Your bank account has $100, you spent $20 on lunch, $50 on dinner, and earn $200 from your job. Calculate how much money you have.
+100 - 20 - 50 + 200;
+
+//  1j. Use document.body.innerHTML=...; to make the webpage blank.
+document.body.innerHTML = "";
