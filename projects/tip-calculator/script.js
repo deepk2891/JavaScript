e27849d6ecbf6f78d3ed680c975d6990cf570fe9
Deep Kothari <deepk2891@gmail.com>
2023-12-02 23:43:09 +0530

    projects tip-calculator

---
+++ projects/tip-calculator/script.js
@@ -0,0 +1,18 @@
+/* 
+🌟 APP: Tip Calculator
+
+These are the 3 functions you must use 👇
+========================================
+calculateBill()
+increasePeople()
+decreasePeople()
+
+These functions are hard coded in the HTML. So, you can't change their names.
+
+These are all the DIV ID's you're gonna need access to 👇
+========================================================
+#1 ID 👉 'billTotalInput' = User input for bill total
+#2 ID 👉 'tipInput' = User input for tip
+#3 ID 👉 'numberOfPeople' = Current number of people you're splitting the bill between
+#4 ID 👉 'perPersonTotal' = Total dollar value owed per person
+*/
