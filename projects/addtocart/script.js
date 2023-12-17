94998a0b766a0397394b9af998a6822c4b4c94cb
Deep Kothari <deepk2891@gmail.com>
2023-12-17 16:51:40 +0530

    addtocart display products in HTML using DOM

--- projects/addtocart/script.js
+++ projects/addtocart/script.js
@@ -12,12 +12,44 @@ closeCart.addEventListener('click',() => {
 let listProductHTML = document.querySelector(".listProduct")
 let listProducts = []
 
+const addDataToHTML = () => {
+    listProductHTML.innerHTML = ''
+    if (listProducts.length > 0)
+    {
+        listProducts.forEach(product => {
+            let newProduct = document.createElement('div')
+            newProduct.classList.add('item')
+            newProduct.dataset.id = product.id
+
+            newProduct.innerHTML = `
+                        <img src="${ product.image }" alt="image"/>
+                        <h2>${ product.name }</h2>
+                        <div class="price">₹${ product.price }</div>
+                        <button class="addCart">
+                            Add To Cart
+                        </button>
+                    `
+            listProductHTML.appendChild(newProduct)
+        })
+    }
+}
+
+listProductHTML.addEventListener('click',(event) => {
+    let positionClick = event.target
+    if (positionClick.classList.contains("addCart"))
+    {
+        let product_id = positionClick.parentElement.dataset.id
+        alert(product_id)
+    }
+})
+
 const initApp = () => {
     fetch("products.json")
         .then(response => response.json())
         .then(data => {
             listProducts = data
             console.log(listProducts)
+            addDataToHTML()
         })
 }
 initApp()
